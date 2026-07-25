c8b1aa8a459c06f9447fae2cc62e21ef3dc7c033
artem-komar <artem.komar.25@gmail.com>
2026-07-25 20:08:32 +0300

    Додано розрахунок коефіцієнтів зростання, а також обчислено їхню накопичувальну дохідність через послідовне перемноження коефіцієнтів методом .cumprod(), розраховано вартість портфоліо.

--- p&l.py
+++ p&l.py
@@ -10,3 +10,4 @@ market_growth_factor= 1 + df["Market_Return"]
 strategy_growth_factor= 1 + df["Strategy_Return"]
 df["Cum_Market_Return"]= market_growth_factor.cumprod()- 1
 df["Cum_Strategy_Return"]= strategy_growth_factor.cumprod()- 1
+df["Portfolio_Value"]=(start_capital *(1+df["Cum_Strategy_Return"])).round(2)
