457b7c11c81f5726f633f4325fcdee5233265182
maxander <andrii.maksymov.25@ske.org.ua>
2026-07-25 17:44:55 +0300

    Здійснив розрахунок обох середніх ковзних, і додав їхні значення до датасету

--- signals.py
+++ signals.py
@@ -4,3 +4,8 @@ df = pd.read_csv("brk_-_b_data.csv", index_col=0, parse_dates=True)
 print(df)
 short_window = 20
 long_window = 100
+
+#Розрахунок ковзних за допомогою .rolling().mean()
+
+df["MA_short"] = df["Close"].rolling(window=short_window).mean()
+df["MA_long"] = df["Close"].rolling(window=long_window).mean()
