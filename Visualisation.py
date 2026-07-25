53772df768b7c3e3ec9802c35b69d420866a3812
maxander <andrii.maksymov.25@ske.org.ua>
2026-07-25 23:37:41 +0300

    Додав генерацію зображення графіку

--- Visualisation.py
+++ Visualisation.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("p&l.py", index_col=0, parse_dates=True)
+df = pd.read_csv("pnl_results.csv", index_col=0, parse_dates=True)
 
 plt.figure(figsize=(14, 10))
 plt.style.use('seaborn-v0_8-darkgrid')
+
+plt.tight_layout()
+output_filename = 'strategy_visualization.png'
+plt.savefig(output_filename, dpi=300)
+plt.close()
+
+print(f"Графік успішно збережено у файл: {output_filename}")
