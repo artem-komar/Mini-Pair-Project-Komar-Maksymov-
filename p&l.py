509228d5b6d3d8503413ef69f0c81b369b1e63a6
artem-komar <artem.komar.25@gmail.com>
2026-07-25 20:03:53 +0300

    Додано розрахунок коефіцієнтів зростання, а також обчислено їхню накопичувальну дохідність через послідовне перемноження коефіцієнтів методом .cumprod()

--- p&l.py
+++ p&l.py
@@ -5,3 +5,8 @@ start_capital=10000.0
 
 df["Market_Return"] = df["Close"].pct_change()
 df["Strategy_Return"] = df["Market_Return"]*df["Position"].shift(1)
+
+market_growth_factor= 1 + df["Market_Return"]
+strategy_growth_factor= 1 + df["Strategy_Return"]
+df["Cum_Market_Return"]= market_growth_factor.cumprod()- 1
+df["Cum_Strategy_Return"]= strategy_growth_factor.cumprod()- 1
