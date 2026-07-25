10c465c3003114889a78283e843c1b429ec19016
artem-komar <artem.komar.25@gmail.com>
2026-07-25 14:18:56 +0300

    Додано знак валюти

--- calculations.py
+++ calculations.py
@@ -7,10 +7,12 @@ end_date="2026-07-22"
 ticker_name="BRK-B"
 exchange="NYSE (New York Stock Exchange)"
 currency="USD"
+sign="$"
 
 print(f"Обраний актив: {ticker_name}")
 print(f"Біржа: {exchange}")
 print(f"Валюта: {currency}")
+print(f"Знак: {sign}")
 print(f"Отримані дані з {start_date} по {end_date} включно.")
 raw_data=yf.download(ticker_name, start=start_date, end=end_date,progress=False)
 
