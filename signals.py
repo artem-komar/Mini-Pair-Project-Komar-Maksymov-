3513ef40ff2329db5425b20087c5716f36a12952
maxander <andrii.maksymov.25@ske.org.ua>
2026-07-25 18:52:23 +0300

    Додав систему оцінювання сигналів відносно зміни сигналів, що прораховується через позицію сигналу

--- signals.py
+++ signals.py
@@ -12,5 +12,13 @@ df["MA_long"] = df["Close"].rolling(window=long_window).mean()
 df["Position"] = 0
 df.loc[df["MA_short"] > df["MA_long"], "Position"] = 1
 
+df["ssignal_change"] = df["Position"].diff()
+
+#Вводимо сигнали, аби зараз усі були на Hold
+df["Signal"] = "Hold"
+#Ввів умови того, якими стають бути сигнали відповідно того як змінюється перетин ковзних
+df.loc[df["ssignal_change"] == 1, "Signal"] = "Buy"
+df.loc[df["ssignal_change"] == -1, "Signal"] = "Sell"
+
 df = df.dropna(subset=["MA_long"]).copy()
 print(df)
