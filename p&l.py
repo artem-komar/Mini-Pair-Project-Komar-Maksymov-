6edd3fe8d4a8603f6b02f36bd6cda0e5d4c04289
artem-komar <artem.komar.25@gmail.com>
2026-07-25 20:19:07 +0300

    Розраховано підсумкові показники ефективності стратегії, де за допомогою методу .iloc() витягнуто останні значення з таблиці, а показники дохідності ринку та стратегії переведено у відсотки шляхом домноження на 100 з обчисленням підсумкового балансу чистого прибутку та кількості угод

--- p&l.py
+++ p&l.py
@@ -11,3 +11,9 @@ strategy_growth_factor= 1 + df["Strategy_Return"]
 df["Cum_Market_Return"]= market_growth_factor.cumprod()- 1
 df["Cum_Strategy_Return"]= strategy_growth_factor.cumprod()- 1
 df["Portfolio_Value"]=(start_capital *(1+df["Cum_Strategy_Return"])).round(2)
+
+total_market_return= df["Cum_Market_Return"].iloc[-1] * 100
+total_strategy_return= df["Cum_Strategy_Return"].iloc[-1] * 100
+final_balance= df["Portfolio_Value"].iloc[-1]
+net_profit= final_balance -start_capital
+total_trades=sum(df["Signal"]=="Buy")
